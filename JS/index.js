eb4ae027a9d2118e9ec1101b3b2770ea67f80845
d4nQw3rty <danlinmoy@gmail.com>
2023-05-01 10:30:22 -0300

    handle click event on squares

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,31 @@
 const App = {
   $:{
-    menu: document.querySelector('.menu'),
-    menuItems: document.querySelector('.items'),
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]')
   },
   init(){
+        
     App.$.menu.addEventListener('click', (event) => {
       App.$.menuItems.classList.toggle('hidden');
     });
+
+    App.$.resetBtn.addEventListener('click', event => {
+        console.log('reset the game');
+    });
+
+    App.$.newRoundBtn.addEventListener('click', event => {
+        console.log('new game');
+    });
+
+    App.$.squares.forEach( square => {
+        square.addEventListener('click', event => {
+           console.log(`Square with id: ${event.target.id} was clicked`)
+        })
+    })
   }
 }
 
-window.addEventListener('load', () => App.init())
+window.addEventListener('load', App.init)
